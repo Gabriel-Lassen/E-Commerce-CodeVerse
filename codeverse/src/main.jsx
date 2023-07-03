28c1f02b6fb1869485e09e0328899e1e02fb75d5
lucas <lucaspsckenal@gmail.com>
2023-07-03 15:44:57 -0300

    docs: routes

--- codeverse/src/main.jsx
+++ codeverse/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './global.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Rotas from "./routes/routes";
+import "./global.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Rotas />
+  </React.StrictMode>
+);
