5557394a49cf7b44aca306c0948fd1481e7b086b
lucas <lucaspsckenal@gmail.com>
2023-07-05 15:19:12 -0300

    add: switch heroBannerHome and rework responsiveness

--- codeverse/src/components/heroBanner-home/index.jsx
+++ codeverse/src/components/heroBanner-home/index.jsx
@@ -1,31 +1,22 @@
 import styles from "./styles.module.scss";
-import banner1 from "../../assetes/imgs/heroBannerHome.png";
-import banner2 from "../../assetes/imgs/heroBannerHome2.png";
 import arrow from "../../assetes/imgs/icon-arrow.svg";
+import Banner1 from "../../assetes/imgs/heroBannerHomeMob.png";
+import Banner2 from "../../assetes/imgs/heroBannerHomeMob2.png";
 
 const HeroBannerHome = () => {
   return (
     <div className={styles.HeroBannerHomeContainer}>
-      <div className={styles.banner1}>
-        <img src={banner1} alt="" />
-        <div className={styles.description}>
-          <h2>Carry your Funk</h2>
-          <p>Trendy handbags collection for your party animal</p>
-
-          <button className={styles.descriptionBtn}>
-            <img src={arrow} />
-            See more
-          </button>
-        </div>
+      <div className={styles.HeroBannerHomeMob}>
+        <img src={Banner1} alt="" />
       </div>
-      <div className={styles.banner2}>
-        <img src={banner2} alt="" />
-        <div className={styles.description_2}>
-          <h2>Spring Summer Collection</h2>
-          <div className={styles.promo}>
-            <h3>UPTO 20% OFF</h3>
-          </div>
-        </div>
+      <div className={styles.HeroBannerHomeWeb}>
+        <button className={styles.descriptionBtn}>
+          <img src={arrow} alt="" />
+          see more
+        </button>
+      </div>
+      <div className={styles.HeroBannerHomeMob2}>
+        <img src={Banner2} alt="" />
       </div>
     </div>
   );
