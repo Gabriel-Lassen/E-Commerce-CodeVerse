6a540da0c520c785252e3f4ef6daaf6d29b30e45
lucas <lucaspsckenal@gmail.com>
2023-07-10 15:54:42 -0300

    feat: Top Categories CVS-07

--- codeverse/src/pages/Home/index.jsx
+++ codeverse/src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import HeroBannerHome from "../../components/heroBanner-home";
+import TopCategories from "../../components/TopCategories-home";
 
 const Home = () => {
-  return  (
+  return (
     <>
       <Header />
       <HeroBannerHome />
+      <TopCategories />
       <Footer />
     </>
   );
