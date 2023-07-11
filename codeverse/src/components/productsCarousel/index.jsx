4b6a03afc9f30f54909b3972f8e1b14f9e5d5c34
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-07-11 14:45:15 -0300

    feat:New Arrivals - CVS-08

---
+++ codeverse/src/components/productsCarousel/index.jsx
@@ -0,0 +1,51 @@
+import styles from './styles.module.scss';
+import { ProductsContext } from '../../contexts/products';
+import { useContext, useEffect, useState } from 'react'
+import ProductCard from '../productCard';
+import Chevron from '../../assets/imgs/chevron-right.svg'
+
+const ProductsCarousel = () => {
+  
+  const { listProducts } = useContext(ProductsContext);
+  const [newArrivals, setNewArrivals] = useState();
+
+  useEffect(() => {
+    if(listProducts){
+      const list = listProducts.filter((item) => {return item.releaseDate.indexOf('2023-06') !== -1});
+      setNewArrivals(list);
+    }
+  },[listProducts])
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.viewAll}>
+        <h2>New Arrivals</h2>
+        <button>
+          <span>View All</span>
+          <img src={Chevron} alt="" />
+        </button>
+      </div>
+      <div className={styles.carousel}>
+        {newArrivals && 
+          newArrivals.map((item) => {
+            return <ProductCard
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              info={item.info}
+              price={item.price}
+              discount={item.discount}
+              averageStars={item.rating.averageStars}
+              totalRatings={item.rating.totalRatings}
+              url={item.url}
+              popularity={item.popularity}
+              addToBagBtn={false}
+            />
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default ProductsCarousel
--- codeverse/src/pages/Home/index.jsx
+++ codeverse/src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import HeroBannerHome from "../../components/heroBanner-home";
+import ProductsCarousel from "../../components/productsCarousel";
 
 const Home = () => {
   return  (
     <>
       <Header />
       <HeroBannerHome />
+      <ProductsCarousel />
       <Footer />
     </>
   );
