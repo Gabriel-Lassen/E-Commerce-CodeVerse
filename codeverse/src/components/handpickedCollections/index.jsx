475f953ed6f2b2b3a24a1b833ae5791d266a8932
Nicolle Poltosi <nimendespoltosi46@gmail.com>
2023-07-11 22:11:15 -0300

    updates and imgs

--- codeverse/src/components/handpickedCollections/index.jsx
+++ codeverse/src/components/handpickedCollections/index.jsx
@@ -1,7 +1,31 @@
 import styles from "./styles.module.scss";
+import img1 from "../../assets/imgs/handbags.png";
+import img2 from "../../assets/imgs/personal care.png";
+import img3 from "../../assets/imgs/sunglasses.png";
+import img4 from "../../assets/imgs/wrist watches.png";
 
 function Handpicked() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <p>Handpicked Collections</p>
+      <div className={styles.column}>
+        <a href="//www.google.com">
+          <img src={img2} alt="" />
+        </a>
+        <a href="">
+          <img src={img1} alt="" />
+        </a>
+        <div className={styles.column}>
+          <a href="">
+            <img src={img4} alt="" />
+          </a>
+          <a href="">
+            <img src={img3} alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Handpicked;
