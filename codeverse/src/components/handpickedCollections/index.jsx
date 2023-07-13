59a464f10a6cec74b9a825146234ae99181836c8
Nicolle Poltosi <nimendespoltosi46@gmail.com>
2023-07-13 14:02:53 -0300

    proposed improvements in the review

--- codeverse/src/components/handpickedCollections/index.jsx
+++ codeverse/src/components/handpickedCollections/index.jsx
@@ -1,4 +1,5 @@
 import styles from "./styles.module.scss";
+import { Link } from "react-router-dom";
 import imag1 from "../../assets/imgs/handbags.png";
 import imag2 from "../../assets/imgs/personalcare.png";
 import imag3 from "../../assets/imgs/sunglasses.png";
@@ -10,20 +11,20 @@ function Handpicked() {
       <p>Handpicked Collections</p>
       <div className={styles.all}>
         <div className={styles.column}>
-          <a href="">
+          <Link to="/">
             <img src={imag2} alt="" />
-          </a>
-          <a href="">
+          </Link>
+          <Link to="/">
             <img src={imag1} alt="" />
-          </a>
+          </Link>
         </div>
         <div className={styles.column}>
-          <a href="">
+          <Link to="/">
             <img src={imag4} alt="" />
-          </a>
-          <a href="">
+          </Link>
+          <Link to="/">
             <img src={imag3} alt="" />
-          </a>
+          </Link>
         </div>
       </div>
     </div>
