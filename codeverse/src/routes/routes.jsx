5f96782e1bb5a3c747efcb35d6807ddca3344e8f
Gabriel Lassen dos Santos <gabriel.lassen2005@gmail.com>
2023-07-17 17:53:03 -0300

    add: Routes

--- codeverse/src/routes/routes.jsx
+++ codeverse/src/routes/routes.jsx
@@ -1,11 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import MyCart from "../pages/Mycart";
+import WishList from "../pages/Wishlist";
+import Profile from "../pages/Profile";
+import Categories from "../pages/Categories";
+import Products from "../pages/Products";
 
 const Rotas = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/products/:id" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mycart" element={<MyCart />} />
+        <Route path="/wishlist" element={<WishList />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
