3ea763c5e1321e0b230cf7ed22427b25505839b9
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-18 15:35:25 -0300

    add: react icons

--- codeverse/src/pages/Login/index.jsx
+++ codeverse/src/pages/Login/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.scss";
-
+import { GoMail, GoLock } from "react-icons/go";
 
 const Login = () => {
   return (
@@ -10,10 +10,12 @@ const Login = () => {
         <h1>Welcome Back!</h1>
         <div className={styles.input_bx}>
           <input type="text" required="required" placeholder="Email" />
+          <GoMail/>
           {/* <span>Email</span> */}
         </div>
         <div className={styles.input_bx}>
           <input type="password" required="required" placeholder="Password" />
+          <GoLock/>
           {/* <span>Password</span> */}
         </div>
         <div className={styles.login}>
