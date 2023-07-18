ced25ce18eacf0de9acc6735c1b7ef3529ae44b5
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-18 20:37:22 -0300

    register page structure

--- codeverse/src/pages/Signup/index.jsx
+++ codeverse/src/pages/Signup/index.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import styles from "./styles.module.scss";
+import user from '../../assets/imgs/imgUser.jpg'
+
 function Signup() {
   return (
-    <div>Signup</div>
+    <div className={styles.container}>
+      <div className={styles.user}>
+        <img src={user} alt="" />
+        <button>Upload</button>
+      </div>
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <div>
+          <input type="email" name="" id="" />
+          <input type="date" name="" id="" />
+          <input type="password" />
+          <input type="password" />
+        </div>
+      </div>
+    </div>
   )
 }
 
