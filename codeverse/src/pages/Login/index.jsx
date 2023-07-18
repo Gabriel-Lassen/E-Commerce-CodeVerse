bcdd6ff68b6281e71a2898768ef3c2041cfa4a6d
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-17 22:47:21 -0300

    feat: mobile page style

--- codeverse/src/pages/Login/index.jsx
+++ codeverse/src/pages/Login/index.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./styles.module.scss";
 
-import styles from './styles.module.scss'
 const Login = () => {
   return (
-    
-    <div className={styles.login}>
-    
+    <div className={styles.container}>
+      <div>
+        <h1>Welcome Back!</h1>
+        <div className={styles.input_bx}>
+          <input type="text" required="required" placeholder="email" />
+          {/* <span>Email</span> */}
+        </div>
+        <div className={styles.input_bx}>
+          <input type="password" required="required" placeholder="password" />
+          {/* <span>Password</span> */}
+        </div>
+        <div className={styles.login}>
+          <button>Sign in</button>
+          <p>Dont have an account? <Link className={styles.link}>Sign up</Link></p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
