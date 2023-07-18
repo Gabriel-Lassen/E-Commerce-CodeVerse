d7009bddf4842421b3df2b35b73e3b97a3384097
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-18 20:50:36 -0300

    add: image user

--- codeverse/src/pages/Signup/index.jsx
+++ codeverse/src/pages/Signup/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import styles from "./styles.module.scss";
-import user from '../../assets/imgs/imgUser.jpg'
+import user from '../../assets/imgs/imagemUser.jpg'
 
 function Signup() {
   return (
