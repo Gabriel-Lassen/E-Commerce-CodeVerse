f601c1a0a9724aab300f10f895fbd8c82138e631
Nicolle Poltosi <nimendespoltosi46@gmail.com>
2023-07-19 18:09:58 -0300

    updates styles

--- codeverse/src/pages/Categories/index.jsx
+++ codeverse/src/pages/Categories/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from "./styles.modules.scss";
+import styles from "../Categories/styles.module.scss";
+import { Link } from "react-router-dom";
 import skincare from "../../assets/imgs/category-skincare.png";
 import fragrance from "../../assets/imgs/category-fragrance.png";
 import handbags from "../../assets/imgs/category-handbags.png";
@@ -7,7 +8,30 @@ import eyewear from "../../assets/imgs/category-eyewear.png";
 import apparels from "../../assets/imgs/category-apparels.png";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <h1>Categories</h1>
+      </div>
+      <div className={styles.categorys}>
+        <Link to="/">
+          <img src={skincare} alt="" />
+        </Link>
+        <Link to="/">
+          <img src={fragrance} alt="" />
+        </Link>
+        <Link to="/">
+          <img src={handbags} alt="" />
+        </Link>
+        <Link to="/">
+          <img src={eyewear} alt="" />
+        </Link>
+        <Link to="/">
+          <img src={apparels} alt="" />
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Categories;
