78d6b57190bd82c29b91af900a8f71127e36988d
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-18 23:35:51 -0300

    mobile register page style

--- codeverse/src/pages/Signup/index.jsx
+++ codeverse/src/pages/Signup/index.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./styles.module.scss";
-import user from '../../assets/imgs/imagemUser.jpg'
+import user from "../../assets/imgs/imagemUser.jpg";
 
 function Signup() {
   return (
     <div className={styles.container}>
       <div className={styles.user}>
-        <img src={user} alt="" />
-        <button>Upload</button>
-      </div>
-      <div>
-        <input type="text" />
-        <input type="text" />
-        <div>
-          <input type="email" name="" id="" />
-          <input type="date" name="" id="" />
-          <input type="password" />
-          <input type="password" />
+        <h1>Create Account</h1>
+        <div className={styles.avatar}>
+          <img src={user} alt="" />
+          <button>Upload</button>
         </div>
       </div>
+      <label>
+        <span>First name:</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Last name:</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span> Date of birth: </span>
+        <input type="date" />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input type="password" />
+      </label>
+      <label>
+        <span>Confirm password:</span>
+        <input type="password" />
+        <button>Sign Up</button>
+      </label>
     </div>
-  )
+  );
 }
 
 export default Signup;
