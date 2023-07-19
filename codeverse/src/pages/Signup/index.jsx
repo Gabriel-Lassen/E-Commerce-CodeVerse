d8ee9470d78f13203a8218b7657889cbe313f9b3
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-19 10:45:49 -0300

    desktop page style

--- codeverse/src/pages/Signup/index.jsx
+++ codeverse/src/pages/Signup/index.jsx
@@ -6,38 +6,44 @@ import user from "../../assets/imgs/imagemUser.jpg";
 function Signup() {
   return (
     <div className={styles.container}>
-      <div className={styles.user}>
-        <h1>Create Account</h1>
-        <div className={styles.avatar}>
-          <img src={user} alt="" />
-          <button>Upload</button>
+      <div className={styles.modal}>
+        <div className={styles.user}>
+          <h1>Create Account</h1>
+          <div className={styles.avatar}>
+            <img src={user} alt="" />
+            <button> Upload</button>
+          </div>
+        </div>
+        <div className={styles.labels}>
+        <div className={styles.row}>
+          <label>
+            <span>First name:</span>
+            <input type="text" />
+          </label>
+          <label>
+            <span>Last name:</span>
+            <input type="text" />
+          </label>
+        </div>
+        <label>
+          <span>Email:</span>
+          <input type="text" />
+        </label>
+        <label>
+          <span> Date of birth: </span>
+          <input type="date" />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input type="password" />
+        </label>
+        <label>
+          <span>Confirm password:</span>
+          <input type="password" />
+          <button>Sign Up</button>
+        </label>
         </div>
       </div>
-      <label>
-        <span>First name:</span>
-        <input type="text" />
-      </label>
-      <label>
-        <span>Last name:</span>
-        <input type="text" />
-      </label>
-      <label>
-        <span>Email:</span>
-        <input type="text" />
-      </label>
-      <label>
-        <span> Date of birth: </span>
-        <input type="date" />
-      </label>
-      <label>
-        <span>Password:</span>
-        <input type="password" />
-      </label>
-      <label>
-        <span>Confirm password:</span>
-        <input type="password" />
-        <button>Sign Up</button>
-      </label>
     </div>
   );
 }
