9dc4cf24a80e393dfdaee18ac4a20d6ca9e917fb
Nicolle Poltosi <nimendespoltosi46@gmail.com>
2023-07-19 14:05:11 -0300

    Add imgs and file

--- codeverse/src/pages/Categories/index.jsx
+++ codeverse/src/pages/Categories/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import styles from "./styles.modules.scss";
+import skincare from "../../assets/imgs/category-skincare.png";
+import fragrance from "../../assets/imgs/category-fragrance.png";
+import handbags from "../../assets/imgs/category-handbags.png";
+import eyewear from "../../assets/imgs/category-eyewear.png";
+import apparels from "../../assets/imgs/category-apparels.png";
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
-}
+  return <div>Categories</div>;
+};
 
 export default Categories;
