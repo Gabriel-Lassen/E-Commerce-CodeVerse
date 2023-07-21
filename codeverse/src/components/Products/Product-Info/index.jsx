c48ecb1c24b8bd1144d4de7bffbe829e1eb0ca35
lucas <lucaspsckenal@gmail.com>
2023-07-21 01:39:31 -0300

    add: static web page

--- codeverse/src/components/Products/Product-Info/index.jsx
+++ codeverse/src/components/Products/Product-Info/index.jsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../../contexts/products";
+import Star from "../../../assets/imgs/star.svg";
 
 const ProductsInfo = () => {
   const { listProducts } = useContext(ProductsContext);
@@ -9,6 +10,9 @@ const ProductsInfo = () => {
   const [price, setPrice] = useState("");
   const [qty, setQty] = useState("");
   const [discount, setDiscount] = useState("");
+  const [rating, setRating] = useState("");
+  const [totalRatings, setTotalRatings] = useState("");
+  const [averageStars, setAverageStars] = useState("");
 
   useEffect(() => {
     if (listProducts) {
@@ -22,18 +26,41 @@ const ProductsInfo = () => {
         setPrice(product.price);
         setQty(product.qty);
         setDiscount(product.discount);
+        setQty(product.qty);
+        setRating(product.rating);
+        setTotalRatings(product.totalRatings);
+        setAverageStars(product.averageStars);
       }
     }
   }, [listProducts]);
+
+  let stars = [];
+  for (let i = 0; i < averageStars; ++i) {
+    stars.push(<img src={Star} key={i} />);
+  }
+
   return (
     <div className={styles.container}>
       <h2>{name}</h2>
       <p>{description}</p>
       <div className={styles.priceContainer}>
-        <h2>{price}</h2>
-        <p></p>
-        <p>{discount}</p>
-        <div className={styles.rating}></div>
+        <h2>${(price * (1 - discount)).toFixed(2)}</h2>
+        <span>
+          <s>${price}</s>
+        </span>
+        <p>{discount * 100}% OFF</p>
+      </div>
+      {rating && (
+        <div className={styles.rating}>
+          <div className={styles.stars}>{stars}</div>
+          <span>{totalRatings} Ratings</span>
+        </div>
+      )}
+      <div className={styles.qty}>
+        <span>Quantity:</span>
+        <div className={styles.btn}>
+          <button>-</button>1<button>+</button>
+        </div>
       </div>
     </div>
   );
--- codeverse/src/pages/Products/index.jsx
+++ codeverse/src/pages/Products/index.jsx
@@ -1,14 +1,30 @@
+import styles from "./styles.module.scss";
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import ProductsInfo from "../../components/Products/Product-Info";
 import ProductImg from "../../components/Products/Products-imgs";
+import { useState, useEffect } from "react";
+
 const Products = () => {
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    const handleHidden = () => {
+      setHidden(window.innerWidth >= 768);
+    };
+
+    handleHidden();
+
+    window.addEventListener("resize", handleHidden);
+  });
   return (
     <>
-      <Header />
-      <ProductImg />
-      <ProductsInfo />
-      <Footer />
+      {hidden && <Header />}
+      <div className={styles.teste}>
+        <ProductImg />
+        <ProductsInfo />
+      </div>
+      {hidden && <Footer />}
     </>
   );
 };
