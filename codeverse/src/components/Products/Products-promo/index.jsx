b82c3f71f79d2b950980675c1ddad39eb505a5da
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-07-21 14:17:40 -0300

    fix: add correct component structure

--- codeverse/src/components/Products/Products-promo/index.jsx
+++ codeverse/src/components/Products/Products-promo/index.jsx
@@ -4,6 +4,16 @@ const ProductPromo = () => {
  
   return (
     <>
+      <div className={styles.wrapper}>
+        <div className={styles.cardInfo}>
+          <span className={styles.infoDiscount}></span>
+          <span className={styles.infoPolicy}></span>
+        </div>
+        <div className={styles.cardCode}>
+          <span className={styles.codeUser}></span>
+          <span className={styles.codeNumber}></span>
+        </div>
+      </div>
     </>
   );
 };
