903d2bacdf214522d5c836356311b1f22d840579
Gabriel Lassen dos Santos <gabriel.lassen2005@gmail.com>
2023-07-20 21:17:33 -0300

    feat:Promo-CVS-21

--- codeverse/src/pages/Products/index.jsx
+++ codeverse/src/pages/Products/index.jsx
@@ -1,11 +1,17 @@
 import Footer from "../../components/footer";
 import Header from "../../components/header";
 import ProductImg from "../../components/Products/Products-imgs";
+import ProductPromo from "../../components/Products/Products-promo";
+
+import styles from "./styles.module.scss";
 const Products = () => {
   return (
     <>
       <Header />
+      <div className={styles.wrapper}>
       <ProductImg />
+      <ProductPromo/>
+      </div>
       <Footer />
     </>
   );
