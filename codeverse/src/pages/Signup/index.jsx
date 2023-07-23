2adb83512e584ec79505f8971799c144843d716e
lucas <lucaspsckenal@gmail.com>
2023-07-23 13:47:45 -0300

    fix: styles p

--- codeverse/src/pages/Signup/index.jsx
+++ codeverse/src/pages/Signup/index.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import styles from "./styles.module.scss";
+import user from "../../assets/imgs/imagemUser.jpg";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/Auth";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
 function Signup() {
-  const {register} = useContext(AuthContext)
-
+  const { register } = useContext(AuthContext);
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -10,27 +14,23 @@ function Signup() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [birthdate, setBirthdate] = useState("");
-  const [avatar, setAvatar] = useState(null)
-  const [imageAvatar, setImageAvatar] = useState(null)
-
+  const [avatar, setAvatar] = useState(null);
+  const [imageAvatar, setImageAvatar] = useState(null);
 
   function handleFile(e) {
-    if(e.target.files[0]) {
-      const image = e.target.files[0]
+    if (e.target.files[0]) {
+      const image = e.target.files[0];
 
-
-      if (image.type === 'image/jpeg' || image.type === 'image/png' ){
-        setImageAvatar(image)
-        setAvatar(URL.createObjectURL(image))
-      }else{
-        toast.warning("Upload a png image")
-        setImageAvatar(null)
+      if (image.type === "image/jpeg" || image.type === "image/png") {
+        setImageAvatar(image);
+        setAvatar(URL.createObjectURL(image));
+      } else {
+        toast.warning("Upload a png image");
+        setImageAvatar(null);
         return;
       }
     }
-
   }
-
 
   function formatBirthdate(input) {
     const onlyNumbers = input.replace(/[^\d]/g, "");
@@ -49,12 +49,10 @@ function Signup() {
     return formattedDate;
   }
 
-
   function handleRegister(e) {
     e.preventDefault();
 
     if (email !== "" && password !== "") {
-
       const currentDate = new Date();
       const birthdateInput = new Date(birthdate);
       const ageDiffMilliseconds = currentDate - birthdateInput;
@@ -66,35 +64,35 @@ function Signup() {
         return;
       }
     }
-      if (
-        email === "" ||
-        password === "" ||
-        firstName === "" ||
-        lastName === "" ||
-        birthdate === "" ||
-        confirmPassword === ""
-      ) {
-        toast.warning("Fill in all fields please");
-        return;
-      }
-      if (!handleEmail(email)) {
-        toast.warning("The email does not meet the requirements");
-        return;
-      }
-      if (!handlePassword(password)) {
-        toast.info("Password does not meet requirements(A!1)");
-        return;
-      }
-      if (password != confirmPassword) {
-        toast.error("Passwords do not match.");
-        return;
-      }
+    if (
+      email === "" ||
+      password === "" ||
+      firstName === "" ||
+      lastName === "" ||
+      birthdate === "" ||
+      confirmPassword === ""
+    ) {
+      toast.warning("Fill in all fields please");
+      return;
+    }
+    if (!handleEmail(email)) {
+      toast.warning("The email does not meet the requirements");
+      return;
+    }
+    if (!handlePassword(password)) {
+      toast.info("Password does not meet requirements(A!1)");
+      return;
+    }
+    if (password != confirmPassword) {
+      toast.error("Passwords do not match.");
+      return;
+    }
 
-      if(imageAvatar === null) {
-        toast.warning("I sent a JPEG or PNG image")
-        return
-      }
-      register(email,password, birthdate, firstName, lastName, imageAvatar)
+    if (imageAvatar === null) {
+      toast.warning("I sent a JPEG or PNG image");
+      return;
+    }
+    register(email, password, birthdate, firstName, lastName, imageAvatar);
   }
 
   function capitalize(string) {
@@ -131,8 +129,81 @@ function Signup() {
   handlePassword();
 
   return (
-    <div>Signup</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <div className={styles.user}>
+          <h1>Create Account</h1>
+          <div className={styles.avatar}>
+            {avatar === null ? (
+              <img src={user} alt="" />
+            ) : (
+              <img src={avatar} alt="" />
+            )}
+            <label>
+              <span>Upload</span>
+              <input type="file" accept="image/*" onChange={handleFile} />
+            </label>
+          </div>
+        </div>
+        <form className={styles.labels} onSubmit={handleRegister}>
+          <div className={styles.row}>
+            <label>
+              <span>First name:</span>
+              <input
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(capitalize(e.target.value))}
+              />
+            </label>
+            <label>
+              <span>Last name:</span>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(capitalize(e.target.value))}
+              />
+            </label>
+          </div>
+          <label>
+            <span>Email:</span>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <label>
+            <span> Date of birth: </span>
+            <input
+              type="text"
+              value={formatBirthdate(birthdate)}
+              onChange={(e) => setBirthdate(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Password:</span>
+            <input
+              type="password"
+              id="password"
+              maxLength="12"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+          <label>
+            <span>Confirm password:</span>
+            <input
+              type="password"
+              maxLength="12"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button>Sign Up</button>
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
