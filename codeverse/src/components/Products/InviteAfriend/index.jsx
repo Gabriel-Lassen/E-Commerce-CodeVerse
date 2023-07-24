111fd01fce7b14dbecd815eb61fa15e1db5e9808
lucas <lucaspsckenal@gmail.com>
2023-07-24 17:31:18 -0300

    refactor: remove p from button and add icons

--- codeverse/src/components/Products/InviteAfriend/index.jsx
+++ codeverse/src/components/Products/InviteAfriend/index.jsx
@@ -1,16 +1,20 @@
-import arrow from "../../../assets/imgs/icon-arrow.svg";
+import arrow from "../../../assets/imgs/arrow.svg";
 import styles from "./styles.module.scss";
+import rectangle from "../../../assets/imgs/rectangle.png";
 
 const InviteAFriend = () => {
   return (
     <div className={styles.container}>
-      <h1>Invite Friends & Earn</h1>
-      <p>Get up to 100 reward points for every friend you invite</p>
-      <img src="" alt="" />
-      <button>
-        <p>Invite Now</p>
-        <img src={arrow} alt="" />
-      </button>
+      <div>
+        <h1>Invite Friends & Earn</h1>
+        <p>Get up to 100 reward points for every friend you invite</p>
+
+        <button>
+          Invite Now
+          <img src={arrow} alt="" />
+        </button>
+      </div>
+      <img src={rectangle} alt="" />
     </div>
   );
 };
