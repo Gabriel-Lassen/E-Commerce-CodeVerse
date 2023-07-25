cf4e60a22ff643c087a199a28dbb82be6e8d9e65
lucas <lucaspsckenal@gmail.com>
2023-07-25 16:44:23 -0300

    add: new feature switch imageUrl for imge on click

--- codeverse/src/components/Products/ProductPreview/index.jsx
+++ codeverse/src/components/Products/ProductPreview/index.jsx
@@ -1,5 +1,8 @@
 import styles from "./styles.module.scss";
 import arrow from "../../../assets/imgs/icon-chevron-right-web.svg";
+import teste from "../../../assets/imgs/teste.png";
+import teste2 from "../../../assets/imgs/teste2.png";
+import teste3 from "../../../assets/imgs/teste3.png";
 import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../../contexts/products";
 
@@ -21,18 +24,40 @@ const Preview = () => {
 
   return (
     <div className={styles.container}>
-      <button>
-        <img src={arrow} className={styles.left} />
-      </button>
-      <div>
-        <img src={imageUrl} alt="" />
-        <img src={imageUrl} alt="" />
-        <img src={imageUrl} alt="" />
-        <img src={imageUrl} alt="" />
+      <div className={styles.arrow}>
+        <button>
+          <img src={arrow} className={styles.left} />
+        </button>
       </div>
-      <button>
-        <img src={arrow} alt="" />
-      </button>
+      <div>
+        <button onClick={() => setImageUrl(`https://placebear.com/g/200/200`)}>
+          <img src={teste} alt="" />
+        </button>
+        <button
+          onClick={() =>
+            setImageUrl(
+              `https://cdn.shopify.com/s/files/1/1830/5085/products/VE0007_BCAA_Capsule_90ct_2048x2048.png?v=1494855182`
+            )
+          }
+        >
+          <img src={teste2} alt="" />
+        </button>
+        <button
+          onClick={() =>
+            setImageUrl(`https://source.unsplash.com/user/c_v_r/1900×800`)
+          }
+        >
+          <img src={teste3} alt="" />
+        </button>
+        <button onClick={() => setImageUrl(imageUrl)}>
+          <img src={imageUrl} alt="" />
+        </button>
+      </div>
+      <div className={styles.arrow}>
+        <button>
+          <img src={arrow} alt="" />
+        </button>
+      </div>
     </div>
   );
 };
