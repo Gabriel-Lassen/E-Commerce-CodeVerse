e39ee1e364b35be0eede8600ea915a8dab01f01f
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-07-25 15:40:48 -0300

    add: ProductRating style

--- codeverse/src/components/ProductRating/index.jsx
+++ codeverse/src/components/ProductRating/index.jsx
@@ -24,47 +24,62 @@ const ProductRating = () => {
   }, [listProducts]);
 
   return (
-    <div>
-      <div>
-        <span>{productName}</span>
-        <span>{productDetails}</span>
+    <div className={styles.wrapper}>
+      <div className={styles.productInfo}>
+        <span className={styles.productName}>{productName}</span>
+        <span className={styles.productDetails}>{productDetails}</span>
       </div>
-      <div>
-        <div>
+      <div className={styles.productRating}>
+        <div className={styles.averageRating}>
           <span>{productRating.averageStars}</span>
           <img src={Star} alt="Average Rating" />
         </div>
         <span>Average Rating</span>
       </div>
-      <div>
-        <div>
+      <div className={styles.ratingStars}>
+        <div className={styles.starQtyWrapper}>
           <span>5.0</span>
-          <div>
-            <div></div>
+          <div className={styles.starQtyDefault}>
+            <div
+              className={styles.starQtyDynamic}
+              style={{width: `${(productRating.fiveStars * 100) / productRating.totalRatings}%`}}>
+              </div>
           </div>
         </div>
-        <div>
+        <div className={styles.starQtyWrapper}>
           <span>4.0</span>
-          <div>
-            <div></div>
+          <div className={styles.starQtyDefault}>
+            <div
+              className={styles.starQtyDynamic}
+              style={{width: `${(productRating.fourStars * 100) / productRating.totalRatings}%`}}>
+              </div>
           </div>
         </div>
-        <div>
+        <div className={styles.starQtyWrapper}>
           <span>3.0</span>
-          <div>
-            <div></div>
+          <div className={styles.starQtyDefault}>
+            <div
+              className={styles.starQtyDynamic}
+              style={{width: `${(productRating.threeStars * 100) / productRating.totalRatings}%`}}>
+              </div>
           </div>
         </div>
-        <div>
+        <div className={styles.starQtyWrapper}>
           <span>2.0</span>
-          <div>
-            <div></div>
+          <div className={styles.starQtyDefault}>
+            <div
+              className={styles.starQtyDynamic}
+              style={{width: `${(productRating.twoStars * 100) / productRating.totalRatings}%`}}>
+              </div>
           </div>
         </div>
-        <div>
+        <div className={styles.starQtyWrapper}>
           <span>1.0</span>
-          <div>
-            <div></div>
+          <div className={styles.starQtyDefault}>
+            <div
+              className={styles.starQtyDynamic}
+              style={{width: `${(productRating.oneStars * 100) / productRating.totalRatings}%`}}>
+              </div>
           </div>
         </div>
       </div>
