3bc3c5b296eaaad1df218dbd1030b99a9d5b4ffa
lucas <lucaspsckenal@gmail.com>
2023-07-25 17:43:54 -0300

    feat: preview Img CVS-26

--- codeverse/src/components/Products/ProductPreview/index.jsx
+++ codeverse/src/components/Products/ProductPreview/index.jsx
@@ -1,14 +1,13 @@
 import styles from "./styles.module.scss";
 import arrow from "../../../assets/imgs/icon-chevron-right-web.svg";
-import teste from "../../../assets/imgs/teste.png";
-import teste2 from "../../../assets/imgs/teste2.png";
-import teste3 from "../../../assets/imgs/teste3.png";
 import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../../contexts/products";
+import ProductImg from "../Products-imgs";
 
 const Preview = () => {
   const { listProducts } = useContext(ProductsContext);
   const [imageUrl, setImageUrl] = useState("");
+  const [selectImage, setSelectImage] = useState("");
 
   useEffect(() => {
     if (listProducts) {
@@ -18,45 +17,59 @@ const Preview = () => {
 
       if (product) {
         setImageUrl(product.url);
+        setSelectImage(product.url);
       }
     }
   }, [listProducts]);
 
+  const switchImg = (newImage) => {
+    setSelectImage(newImage);
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.arrow}>
-        <button>
-          <img src={arrow} className={styles.left} />
-        </button>
-      </div>
-      <div>
-        <button onClick={() => setImageUrl(`https://placebear.com/g/200/200`)}>
-          <img src={teste} alt="" />
-        </button>
-        <button
-          onClick={() =>
-            setImageUrl(
-              `https://cdn.shopify.com/s/files/1/1830/5085/products/VE0007_BCAA_Capsule_90ct_2048x2048.png?v=1494855182`
-            )
-          }
-        >
-          <img src={teste2} alt="" />
-        </button>
-        <button
-          onClick={() =>
-            setImageUrl(`https://source.unsplash.com/user/c_v_r/1900×800`)
-          }
-        >
-          <img src={teste3} alt="" />
-        </button>
-        <button onClick={() => setImageUrl(imageUrl)}>
-          <img src={imageUrl} alt="" />
-        </button>
-      </div>
-      <div className={styles.arrow}>
-        <button>
-          <img src={arrow} alt="" />
-        </button>
+    <div className={styles.imgs}>
+      <ProductImg selectImage={selectImage} />
+      <div className={styles.container}>
+        <div className={styles.arrow}>
+          <button>
+            <img src={arrow} className={styles.left} />
+          </button>
+        </div>
+        <div>
+          <button onClick={() => switchImg(`https://placebear.com/g/200/200`)}>
+            <img src={`https://placebear.com/g/200/200`} alt="" />
+          </button>
+          <button
+            onClick={() =>
+              switchImg(
+                `https://cdn.shopify.com/s/files/1/1830/5085/products/VE0007_BCAA_Capsule_90ct_2048x2048.png?v=1494855182`
+              )
+            }
+          >
+            <img
+              src={`https://cdn.shopify.com/s/files/1/1830/5085/products/VE0007_BCAA_Capsule_90ct_2048x2048.png?v=1494855182`}
+              alt=""
+            />
+          </button>
+          <button
+            onClick={() =>
+              switchImg(`https://source.unsplash.com/user/c_v_r/1900×800`)
+            }
+          >
+            <img
+              src={`https://source.unsplash.com/user/c_v_r/1900×800`}
+              alt=""
+            />
+          </button>
+          <button onClick={() => switchImg(imageUrl)}>
+            <img src={imageUrl} alt="" />
+          </button>
+        </div>
+        <div className={styles.arrow}>
+          <button>
+            <img src={arrow} alt="" />
+          </button>
+        </div>
       </div>
     </div>
   );
