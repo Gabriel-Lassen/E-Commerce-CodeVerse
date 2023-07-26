04f500d5665baf2b142414e3b4c5c676b3e9aa92
lucas <lucaspsckenal@gmail.com>
2023-07-26 15:45:23 -0300

    add: route for getstated

--- codeverse/src/routes/routes.jsx
+++ codeverse/src/routes/routes.jsx
@@ -9,6 +9,7 @@ import Profile from "../pages/Profile";
 import Categories from "../pages/Categories";
 import Products from "../pages/Products";
 import ProductsByCategoy from "../pages/ProductsByCategory";
+import GetStart from "../pages/GetStart";
 
 const Rotas = () => {
   return (
@@ -20,7 +21,7 @@ const Rotas = () => {
       <Route path="/products/:id" element={<Products />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/getstarted" element={''} />
+      <Route path="/getstarted" element={<GetStart/>} />
       <Route path="/mycart" element={<MyCart />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/profile/personalinformation" element={''} />
