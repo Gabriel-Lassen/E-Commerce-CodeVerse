85b67f254c05e632a1365563503dab2e448b14bc
lucas <lucaspsckenal@gmail.com>
2023-08-01 14:43:24 -0300

    add: options for search

--- codeverse/src/components/Search-Modal/index.jsx
+++ codeverse/src/components/Search-Modal/index.jsx
@@ -4,32 +4,64 @@ import ArrowSvg from "../ArrowSvg";
 
 import SearchIcon from "../../assets/imgs/search_desktop.svg";
 
+import { useState, useContext } from "react";
+import { ProductsContext } from "../../contexts/products";
+
 // eslint-disable-next-line react/prop-types
 const Search = ({ active }) => {
+  const [search, setSearch] = useState("");
+  const { listProducts } = useContext(ProductsContext);
+
+  const filteredSearch =
+    search.length > 0
+      ? listProducts.filter(
+          (product) =>
+            product.brand.includes(search) ||
+            product.name.includes(search) ||
+            product.description.includes(search)
+        )
+      : [];
+
+  console.log(listProducts);
+
   const close = () => {
     active(false);
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles.topBar}>
+      <header className={styles.topBar}>
         <button className={styles.btn} onClick={close}>
           <ArrowSvg color="var(--Dark)" direction="left" />
         </button>
         <div>
-          <input type="search" placeholder="Search" id="" />
+          <input
+            type="search"
+            placeholder="Search"
+            list="options"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <datalist id="options">
+            {filteredSearch.map((product) => {
+              return (
+                <option value={product.name} key={product.id}>
+                  {product.name}
+                </option>
+              );
+            })}
+          </datalist>
           <button>
             <img src={SearchIcon} alt="" />
           </button>
         </div>
-      </div>
+      </header>
 
       <div className={styles.recentSearchs}>
         <h2>Recent Searchs</h2>
         <ul>
-          <li>Womens Wrist Watches</li>
-          <li>Chanel Perfumes</li>
-          <li>Claute Bags</li>
+          <li>teste</li>
+          <li>teste</li>
+          <li>teste</li>
         </ul>
       </div>
 
