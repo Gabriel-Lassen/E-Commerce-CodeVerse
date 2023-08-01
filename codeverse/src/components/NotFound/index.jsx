20f36874bd0dd04d8d6f9623c4e40a13d6ce0515
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-07-31 23:59:12 -0300

    add: page structure not found

--- codeverse/src/components/NotFound/index.jsx
+++ codeverse/src/components/NotFound/index.jsx
@@ -1,9 +1,26 @@
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
+import arrow from "../../assets/imgs/arrow.svg";
+import search from "../../assets/imgs/search.png";
+import option from "../../assets/imgs/ellipses.svg";
 
 const NotFound = () => {
   return (
-    <div></div>
-  )
-}
+    <div className={styles.container}>
+      <div>
+        <img src={arrow} />
+        <h1>Search results</h1>
+        <div>
+          <img src={option} />
+        </div>
+        <div>
+          <img src={search} />
+          <h2>Whoops!</h2>
+          <p>We coudn’t find what you’re looking for. Try something else.</p>
+          <button>Back to home</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
