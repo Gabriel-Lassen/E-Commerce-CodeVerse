7f1e77a446815e0f7c3a8566c8f128c538e3bd26
Gabriel Lassen dos Santos <gabriel.lassen2005@gmail.com>
2023-08-03 15:43:33 -0300

    refactor: RouteHistProd

--- codeverse/src/components/RouteHistory-products/index.jsx
+++ codeverse/src/components/RouteHistory-products/index.jsx
@@ -2,15 +2,13 @@ import Arrow from "../../assets/imgs/chevron-right-small.svg";
 import styles from "./styles.module.scss";
 
 import { useEffect, useState, useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { ProductsContext } from "../../contexts/products";
 
 const RouteHistProd = () => {
-  const location = useLocation();
-  const pathnames = location.pathname.split("/");
-
   const { listProducts } = useContext(ProductsContext);
   const [productCategory, setProductCategory] = useState("");
+  const [productName, setProductName] = useState("");
 
   useEffect(() => {
     if (listProducts) {
@@ -20,42 +18,28 @@ const RouteHistProd = () => {
 
       if (product) {
         setProductCategory(product.category);
+        setProductName(product.name);
       }
     }
   }, [listProducts]);
 
-  const breadcrumbLabels = {
-    "": "Home",
-    products:
-      productCategory.charAt(0).toUpperCase() + productCategory.slice(1),
-  };
-
-  const breadcrumbItems = pathnames.map((pathname, index) => {
-    const routeTo = `/${pathnames.slice(1, index + 1).join("/")}`;
-    const label = breadcrumbLabels[pathname] || pathname;
-
-    return { label, link: routeTo };
-  });
+  const productCategoryUpperCase = productCategory.charAt(0).toUpperCase() + productCategory.slice(1);
 
   return (
-    <nav>
-      <ul className={styles.bread}>
-        {breadcrumbItems.map((step, index) => (
-          <li key={index}>
-            <Link
-              to={step.link}
-              className={step.label === "Home" ? styles.home : styles.link}
-            >
-              {step.label}
+    <div className={styles.bread}>
+      <Link className={styles.link} to='/'>
+        <span className={styles.textBlue}>Home</span>
+          <img src={Arrow} alt="Arrow" />
+      </Link>
 
-              {index !== breadcrumbItems.length - 1 && (
-                <img src={Arrow} alt="" />
-              )}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+      <Link className={styles.link} to={`/categories/${productCategory}`}>
+        <span className={styles.textBlue}>
+          {productCategoryUpperCase}</span>
+          <img src={Arrow} alt="Arrow" />
+      </Link>
+
+        <span className={styles.textGray}>{productName}</span>
+    </div>
   );
 };
 
