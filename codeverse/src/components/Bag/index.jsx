ed8b52f084621582194b7d5da028fba7e35900b9
lucas <lucaspsckenal@gmail.com>
2023-08-03 23:07:02 -0300

    add: start cart page

--- codeverse/src/components/Bag/index.jsx
+++ codeverse/src/components/Bag/index.jsx
@@ -1,7 +1,22 @@
 import styles from "./styles.module.scss";
 
 const Bag = () => {
-  return <div>Bag</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerLeft}>
+        <div className={styles.labels}>
+          <h2>Product Name</h2>
+          <div>
+            <h2>Price</h2>
+            <h2>Qty</h2>
+            <h2>Subtotal</h2>
+          </div>
+        </div>
+        <div className={styles.separador}></div>
+      </div>
+      <div className={styles.containerRight}></div>
+    </div>
+  );
 };
 
 export default Bag;
--- codeverse/src/pages/Mycart/index.jsx
+++ codeverse/src/pages/Mycart/index.jsx
@@ -1,9 +1,15 @@
-
+import Bag from "../../components/Bag";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 
 const MyCart = () => {
   return (
-    <div>MyCart</div>
-  )
-}
+    <>
+      <Header />
+      <Bag />
+      <Footer />
+    </>
+  );
+};
 
 export default MyCart;
