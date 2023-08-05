bd54991c55802f81f1cc6a2625a146c064fe87cb
lucas <lucaspsckenal@gmail.com>
2023-08-05 12:13:24 -0300

    Add: mobile page finish

--- codeverse/src/components/Bag/index.jsx
+++ codeverse/src/components/Bag/index.jsx
@@ -1,17 +1,42 @@
 import styles from "./styles.module.scss";
 import background from "../../assets/imgs/frame-6.png";
-import teste from "../../assets/imgs/bag-white.svg";
+import teste from "../../assets/imgs/imgUser.jpg";
 const Bag = () => {
   return (
     <div className={styles.container}>
       <section className={styles.products}>
         <div className={styles.cards}>
-          <img src={teste} alt="" />
+          <div className={styles.imgAndDesc}>
+            <img src={teste} alt="" />
 
-          <div className={styles.prodDesc}>
-            <span>Coach</span>
-            <span>Leather Coach Bag</span>
-            <span>qty</span>
+            <div className={styles.prodDesc}>
+              <span className={styles.name}>Coach</span>
+              <span className={styles.desc}>Leather Coach Bag</span>
+              <div className={styles.qtyContainer}>
+                <p className={styles.qty}>Qty:</p>
+                <span>1</span>
+              </div>
+              <div className={styles.priceContainer}>
+                <h2>$54.69</h2>
+                <span className={styles.originPrice}>
+                  <s>$69.99</s>
+                </span>
+                <p className={styles.off}>20% OFF</p>
+              </div>
+              {/*<div className={styles.priceContainer}>
+              <h2>${(price * (1 - discount)).toFixed(2)}</h2>
+              <span>
+                <s>${price}</s>
+              </span>
+              <p>{discount * 100}% OFF</p>
+            </div>*/}
+            </div>
+          </div>
+          <div className={styles.separator}></div>
+          <div className={styles.btns}>
+            <button className={styles.wish}>Move to Wishlist</button>
+            <div className={styles.column}></div>
+            <button className={styles.remove}>Remove</button>
           </div>
         </div>
       </section>
