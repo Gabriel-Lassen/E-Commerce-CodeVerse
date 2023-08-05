148b95deeab62f89ec418095da63ba1701488d89
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-05 17:02:23 -0300

    add: UserBag functions

--- codeverse/src/contexts/UserActions.jsx
+++ codeverse/src/contexts/UserActions.jsx
@@ -1,12 +1,71 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
+import { setDoc, doc, collection, getDocs, query, deleteDoc } from "@firebase/firestore";
+import { db } from "../FirebaseConection";
+import { AuthContext } from "./Auth";
+import { toast } from "react-toastify";
 
 export const UserActionsContext = createContext({});
 
 const UserActionsProvider = ({children}) => {
+    const { user } = useContext(AuthContext)
+    const [userBag, setUserBag] = useState([]);
+
+    useEffect(() => {
+        if(user){
+            handleGetUserBag();
+        }
+    }, [user])
+
+    async function handleaddToUserBag(productId){
+        await setDoc(doc(db, `/users/${user.uid}/bag`, productId), {
+            productId,
+        })
+        .then(() => {
+            toast.success("Product added to bag");
+            handleGetUserBag();
+        });
+    };
+
+    async function handleGetUserBag() {
+        let list = [];
+        const q = query(collection(db, `/users/${user.uid}/bag`));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((item) => {
+            const newItem = item.data();
+            list = [...list, newItem];
+        });
+        setUserBag(list);
+    };
+
+    async function handleDeleteOneProductUserBag(productId){
+        await deleteDoc(doc(db, `/users/${user.uid}/bag`, productId))
+        .then(() => {
+            toast.success("Product removed from bag");
+            handleGetUserBag();
+        }).catch(() => {
+            toast.error('An error occurred, please try again later');
+        });
+    };
+
+    async function handleDeleteAllProductsUserBag(){
+        const q = query(collection(db, `/users/${user.uid}/bag`));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((product) => {
+            const item = product.data();
+            deleteDoc(doc(db, `/users/${user.uid}/bag`, item.productId));
+        });
+        toast.success("Delete all products");
+        setUserBag([]);
+    };
+
   return (
     <UserActionsContext.Provider 
         value={{
-            
+            userBag,
+            handleaddToUserBag,
+            handleGetUserBag,
+            handleDeleteOneProductUserBag,
+            handleDeleteAllProductsUserBag,
         }}
     >
         {children}
@@ -15,4 +74,4 @@ const UserActionsProvider = ({children}) => {
   )
 }
 
-export default UserActionsProvider
+export default UserActionsProvider;
