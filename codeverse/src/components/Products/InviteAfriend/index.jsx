f28dcdc6060c206c8c8d05552743e24d92ec71e7
Gabriel Lassen dos Santos <gabriel.lassen2005@gmail.com>
2023-08-05 00:41:15 -0300

    add: Link in compoent InviteAFriend

--- codeverse/src/components/Products/InviteAfriend/index.jsx
+++ codeverse/src/components/Products/InviteAfriend/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import arrow from "../../../assets/imgs/arrowLong.svg";
 import styles from "./styles.module.scss";
 import rectangle from "../../../assets/imgs/rectangle.png";
@@ -9,10 +10,12 @@ const InviteAFriend = () => {
         <h1>Invite Friends & Earn</h1>
         <p>Get up to 100 reward points for every friend you invite</p>
 
+      <Link to='/profile/referandearn'>
         <button>
           Invite Now
           <img src={arrow} alt="" />
         </button>
+      </Link>
       </div>
       <img src={rectangle} alt="" className={styles.rectangle}/>
     </div>
