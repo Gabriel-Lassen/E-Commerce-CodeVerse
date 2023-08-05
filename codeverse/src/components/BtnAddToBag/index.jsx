d2a1963cd8c7e0b5ee0ce7d384c52ed53fd0a092
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-05 17:05:14 -0300

    add: implementation of the UserBag functions on the BtnAddToBag

--- codeverse/src/components/BtnAddToBag/index.jsx
+++ codeverse/src/components/BtnAddToBag/index.jsx
@@ -1,16 +1,46 @@
 import styles from './styles.module.scss';
 import Bag from '../../assets/imgs/bag.svg';
 import BagWhite from '../../assets/imgs/bag-white.svg';
-import { useState } from 'react';
+import Close from '../../assets/imgs/remove.svg';
+import { useContext, useEffect, useState } from 'react';
+import { UserActionsContext } from '../../contexts/UserActions';
 
-const BtnAddToBag = ({theme}) => {
-    const [themeClassName, setThemeClassName] = useState(theme === 'light' ? 'light' : 'dark');
-    const [image, setImage] = useState(theme === 'light' ? Bag : BagWhite);
+const BtnAddToBag = ({theme, id}) => {
+  const { handleaddToUserBag, handleDeleteOneProductUserBag, userBag } = useContext(UserActionsContext)
+  const [themeClassName, setThemeClassName] = useState(theme === 'light' ? 'light' : 'dark');
+  const [image, setImage] = useState(theme === 'light' ? Bag : BagWhite);
+  const [isInBag, setIsinBag] = useState(userBag.some(bag => bag.productId === id));
+
+  function handleClick(){
+    if(userBag.some(bag => bag.productId === id)){
+      handleDeleteOneProductUserBag(id);
+      setIsinBag(false);
+    } else {
+      handleaddToUserBag(id);
+      setIsinBag(true);
+    }
+  }
+
+  useEffect(() => {
+    setIsinBag(userBag.some(bag => bag.productId === id));
+  }, [])
+
+  useEffect(() => {
+    setIsinBag(userBag.some(bag => bag.productId === id));
+  }, [userBag])
     
   return (
-    <button className={`${styles.addBagBtn} ${styles[themeClassName]}`}>
-        <img src={image} alt="Add to Bag" />
-        <span>Add to bag</span>
+    <button className={`${styles.addBagBtn} ${styles[themeClassName]}`} onClick={handleClick}>
+        {isInBag ?
+          <img src={Close} alt="Remove from Bag" />
+          :
+          <img src={image} alt="Add to Bag" />
+        }
+        {isInBag ?
+          <span>Remove from bag</span>
+          :
+          <span>Add to bag</span>
+        }
     </button>
   )
 }
