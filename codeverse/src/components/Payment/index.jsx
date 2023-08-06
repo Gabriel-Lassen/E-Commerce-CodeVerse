1951411aa181114f1f0b6e99d296b3799bb00926
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-08-06 02:16:45 -0300

    add: border in enter upi

--- codeverse/src/components/Payment/index.jsx
+++ codeverse/src/components/Payment/index.jsx
@@ -4,7 +4,7 @@ import plusIcon from "../../assets/imgs/small-plus.png";
 import minusIcon from "../../assets/imgs/minus-icon.png";
 import select from "../../assets/imgs/selecionado.png";
 import selectCheckbox from "../../assets/imgs/select.png";
-import googlePlay from '../../assets/imgs/googlePlay.png'
+import googlePlay from "../../assets/imgs/googlePlay.png";
 
 export const Payment = () => {
   const [isContentVisible, setIsContentVisible] = useState(false);
@@ -95,39 +95,52 @@ export const Payment = () => {
       </div>
       {ContentVisible && (
         <>
-        <div className={styles.container}>
+          <div className={styles.container}>
             <div className={styles.contentPayment}>
-                <div className={styles.box}>
-                    <img src={googlePlay} alt="" />
-                </div>
-                <span>Google Pay</span>
+              <div className={styles.box}>
+                <img src={googlePlay} alt="" />
+              </div>
+              <span>Google Pay</span>
             </div>
             <img src={payment === "debit" ? select : selectCheckbox} />
-      </div>
+          </div>
 
-
-      <div className={styles.container}>
+          <div className={styles.container}>
             <div className={styles.contentPayment}>
-                <div className={styles.box}>
-                    <img src={googlePlay} alt="" />
-                </div>
-                <span>Google Pay</span>
+              <div className={styles.box}>
+                <img src={googlePlay} alt="" />
+              </div>
+              <span>Google Pay</span>
             </div>
             <img src={payment === "debit" ? select : selectCheckbox} />
-      </div>
+          </div>
 
-      <div className={styles.container}>
+          <div className={styles.container}>
             <div className={styles.contentPayment}>
-                <div className={styles.box}>
-                    <img src={googlePlay} alt="" />
-                </div>
-                <span>Google Pay</span>
+              <div className={styles.box}>
+                <img src={googlePlay} alt="" />
+              </div>
+              <span>Google Pay</span>
             </div>
             <img src={payment === "debit" ? select : selectCheckbox} />
+          </div>
+        </>
+      )}
+      <div className={styles.contentUPI}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="328"
+          height="142"
+          viewBox="0 0 328 142"
+          fill="none"
+        >
+          <path
+            d="M0.5 16C0.5 12.2146 0.501062 9.4259 0.788435 7.28845C1.07387 5.16538 1.63351 3.75989 2.6967 2.6967C3.75989 1.63351 5.16538 1.07387 7.28845 0.788435C9.4259 0.501062 12.2146 0.5 16 0.5H312C315.785 0.5 318.574 0.501062 320.712 0.788435C322.835 1.07387 324.24 1.63351 325.303 2.6967C326.366 3.75989 326.926 5.16538 327.212 7.28845C327.499 9.4259 327.5 12.2146 327.5 16V126C327.5 129.785 327.499 132.574 327.212 134.712C326.926 136.835 326.366 138.24 325.303 139.303C324.24 140.366 322.835 140.926 320.712 141.212C318.574 141.499 315.785 141.5 312 141.5H16C12.2146 141.5 9.4259 141.499 7.28845 141.212C5.16538 140.926 3.75989 140.366 2.6967 139.303C1.63351 138.24 1.07387 136.835 0.788435 134.712C0.501062 132.574 0.5 129.785 0.5 126V16Z"
+            fill="white"
+            stroke="#F1F1F1"
+          />
+        </svg>
       </div>
-
-      </> 
-)}
     </>
   );
 };
