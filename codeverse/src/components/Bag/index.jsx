a12c7232682679c677c079df8a02744c985c9841
lucas <lucaspsckenal@gmail.com>
2023-08-06 18:25:37 -0300

    add: finish mycart page mobile

--- codeverse/src/components/Bag/index.jsx
+++ codeverse/src/components/Bag/index.jsx
@@ -1,14 +1,24 @@
 import styles from "./styles.module.scss";
 import background from "../../assets/imgs/frame-6.png";
-import teste from "../../assets/imgs/imgUser.jpg";
 import DropdowBtn from "../DropdowBtn";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import Cupom from "../CupomInput";
-
+import { ProductsContext } from "../../contexts/products";
+import { BagActionsContext } from "../../contexts/bagActions";
+import { WishlistActionsContext } from "../../contexts/wishlistActions";
 
 const Bag = () => {
   const [hidden, setHidden] = useState(false);
   const [reverse, setReverse] = useState(true);
+  const { listProducts } = useContext(ProductsContext);
+  const { userBag, handleDeleteOneProductUserBag } =
+    useContext(BagActionsContext);
+  const { handleaddToUserWishlist } = useContext(WishlistActionsContext);
+  const [subTotal, setSubTotal] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [delivery, setDelivery] = useState(0);
+  const [pay, setPay] = useState(0);
+
   useEffect(() => {
     const handleHidden = () => {
       setHidden(window.innerWidth >= 769);
@@ -19,6 +29,31 @@ const Bag = () => {
 
     window.addEventListener("resize", handleHidden);
   });
+
+  const getProductById = (productId) => {
+    return listProducts.find((product) => product.id === productId);
+  };
+
+  const discountedPrice = (productId) => {
+    const product = getProductById(productId);
+    return product.price * (1 - product.discount);
+  };
+
+  useEffect(() => {
+    const Price = userBag.reduce(
+      (totalPrice, item) => totalPrice + discountedPrice(item.productId),
+      0
+    );
+
+    const cupom = 0;
+    const delive = 0;
+    const total = Price + delive;
+
+    setSubTotal(Price);
+    setDiscount(cupom);
+    setDelivery(delive);
+    setPay(total);
+  }, [userBag]);
 
   return (
     <div className={styles.container}>
@@ -32,40 +67,50 @@ const Bag = () => {
           </div>
         </div>
         <div className={styles.separator}></div>
-        <div className={styles.cards}>
-          <div className={styles.imgAndDesc}>
-            <img src={teste} alt="" />
+        {userBag.map((item) => {
+          const product = getProductById(item.productId);
+          if (!product) {
+            return null;
+          }
 
-            <div className={styles.prodDesc}>
-              <span className={styles.name}>Coach</span>
-              <span className={styles.desc}>Leather Coach Bag</span>
-              <div className={styles.qtyContainer}>
-                <p className={styles.qty}>Qty:</p>
-                <span>1</span>
+          const price = discountedPrice(item.productId);
+
+          const handleRemoveFromBag = () => {
+            handleDeleteOneProductUserBag(item.productId);
+          };
+
+          return (
+            <div className={styles.cards} key={product.id}>
+              <div className={styles.imgAndDesc}>
+                <img src={product.url} alt={product.name} />
+
+                <div className={styles.prodDesc}>
+                  <span className={styles.name}>{product.name}</span>
+                  <span className={styles.desc}>{product.info}</span>
+                  <div className={styles.qtyContainer}>
+                    <p className={styles.qty}>Qty:</p>
+                    <span>1</span>
+                  </div>
+                  <div className={styles.priceContainer}>
+                    <h2>${price.toFixed(2)}</h2>
+                    <span className={styles.originPrice}>
+                      <s>{product.price}</s>
+                    </span>
+                    <p className={styles.off}>{product.discount * 100}% OFF</p>
+                  </div>
+                </div>
               </div>
-              <div className={styles.priceContainer}>
-                <h2>$54.69</h2>
-                <span className={styles.originPrice}>
-                  <s>$69.99</s>
-                </span>
-                <p className={styles.off}>20% OFF</p>
+              <div className={styles.separator}></div>
+              <div className={styles.btns}>
+                <button className={styles.wish}>Move to Wishlist</button>
+                <div className={styles.column}></div>
+                <button className={styles.remove} onClick={handleRemoveFromBag}>
+                  Remove
+                </button>
               </div>
-              {/*<div className={styles.priceContainer}>
-              <h2>${(price * (1 - discount)).toFixed(2)}</h2>
-              <span>
-                <s>${price}</s>
-              </span>
-              <p>{discount * 100}% OFF</p>
-            </div>*/}
             </div>
-          </div>
-          <div className={styles.separator}></div>
-          <div className={styles.btns}>
-            <button className={styles.wish}>Move to Wishlist</button>
-            <div className={styles.column}></div>
-            <button className={styles.remove}>Remove</button>
-          </div>
-        </div>
+          );
+        })}
         {reverse && <Cupom />}
 
         {hidden && (
@@ -79,25 +124,25 @@ const Bag = () => {
         <h2>Order Details</h2>
         <div>
           <p>Sub Total</p>
-          <span>$119.69</span>
+          <span>${subTotal.toFixed(2)}</span>
         </div>
         <div>
           <p>Discount</p>
-          <span>-$13.40</span>
+          <span>-${discount}</span>
         </div>
         <div>
           <p>Delivery</p>
-          <span>-$0.00</span>
+          <span>-${delivery.toFixed(2)}</span>
         </div>
         <div>
           <h2>Grand Total</h2>
-          <span>$106.29</span>
+          <span>${pay.toFixed(2)}</span>
         </div>
       </div>
       <div className={styles.bottom}>
         <div>
           <p>Total Bag Amount</p>
-          <span>$106.29</span>
+          <span>${pay.toFixed(2)}</span>
         </div>
         <button>Place Order</button>
       </div>
