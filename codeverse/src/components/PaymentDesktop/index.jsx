38b70f03cda7fb28b1e98e97dfa03bd04f4b1adf
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-06 15:25:17 -0300

    refactor: PaymentDesktop option selected logic

--- codeverse/src/components/PaymentDesktop/index.jsx
+++ codeverse/src/components/PaymentDesktop/index.jsx
@@ -2,15 +2,20 @@ import styles from "./styles.module.scss";
 import select from "../../assets/imgs/selecionado.png";
 import selectCheckbox from "../../assets/imgs/select.png";
 import upi from "../../assets/imgs/UPI-Logo.png";
+import { useState } from "react";
 
 export const PaymentDesktop = () => {
+  const [optionSelected, setOptionSelected] = useState('');
   return (
     <>
     <div className={styles.container}>
       <div className={styles.payment}>
-        <div className={styles.box}>
+        <div className={styles.box} onClick={() => setOptionSelected(optionSelected === 'UPI' ? '' : 'UPI')}
+          style={optionSelected === "UPI" ?
+          {backgroundColor: 'rgba(0, 0, 0, .03)'} : {} }
+        >
           <div className={styles.content}>
-            <img src={select} alt="" />
+            <img src={optionSelected === 'UPI' ? select : selectCheckbox} alt="" />
           </div>
           <div className={styles.center}>
             <img src={upi} alt="" />
@@ -19,9 +24,11 @@ export const PaymentDesktop = () => {
         </div>
       </div>
       <div className={styles.payment}>
-        <div className={styles.box}>
+        <div className={styles.box} onClick={() => setOptionSelected(optionSelected === 'Google' ? '' : 'Google')}
+          style={optionSelected === "Google" ?
+          {backgroundColor: 'rgba(0, 0, 0, .03)'} : {}}>
           <div className={styles.content}>
-            <img src={select} alt="" />
+            <img src={optionSelected === 'Google' ? select : selectCheckbox} alt="" />
           </div>
           <div className={styles.center}>
             <img src={upi} alt="" />
@@ -30,9 +37,11 @@ export const PaymentDesktop = () => {
         </div>
       </div>
       <div className={styles.payment}>
-        <div className={styles.box}>
+        <div className={styles.box} onClick={() => setOptionSelected(optionSelected === 'PAY' ? '' : 'PAY')}
+          style={optionSelected === "PAY" ?
+          {backgroundColor: 'rgba(0, 0, 0, .03)'} : {}}>
           <div className={styles.content}>
-            <img src={select} alt="" />
+            <img src={optionSelected === 'PAY' ? select : selectCheckbox} alt="" />
           </div>
           <div className={styles.center}>
             <img src={upi} alt="" />
@@ -41,9 +50,11 @@ export const PaymentDesktop = () => {
         </div>
       </div>
       <div className={styles.payment}>
-        <div className={styles.box}>
+        <div className={styles.box} onClick={() => setOptionSelected(optionSelected === 'PIX' ? '' : 'PIX')}
+          style={optionSelected === "PIX" ?
+          {backgroundColor: 'rgba(0, 0, 0, .03)'} : {}}>
           <div className={styles.content}>
-            <img src={select} alt="" />
+            <img src={optionSelected === 'PIX' ? select : selectCheckbox} alt="" />
           </div>
           <div className={styles.center}>
             <img src={upi} alt="" />
