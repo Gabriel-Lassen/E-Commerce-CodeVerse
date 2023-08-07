03130b3013ebb11d1858c97796d4d37e6af0513d
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-07 11:22:05 -0300

    fix: BtnAddToBag remove icon color

--- codeverse/src/components/BtnAddToBag/index.jsx
+++ codeverse/src/components/BtnAddToBag/index.jsx
@@ -1,14 +1,16 @@
 import styles from './styles.module.scss';
 import Bag from '../../assets/imgs/bag.svg';
 import BagWhite from '../../assets/imgs/bag-white.svg';
-import Close from '../../assets/imgs/remove.svg';
+import CloseWhite from '../../assets/imgs/remove.svg';
+import Close from '../../assets/imgs/close.svg';
 import { useContext, useEffect, useState } from 'react';
 import { BagActionsContext } from '../../contexts/bagActions';
 
 const BtnAddToBag = ({theme, id}) => {
   const { handleaddToUserBag, handleDeleteOneProductUserBag, userBag } = useContext(BagActionsContext)
   const [themeClassName, setThemeClassName] = useState(theme === 'light' ? 'light' : 'dark');
-  const [image, setImage] = useState(theme === 'light' ? Bag : BagWhite);
+  const [imageBag, setImageBag] = useState(theme === 'light' ? Bag : BagWhite);
+  const [imageClose, setImageClose] = useState(theme === 'light' ? Close : CloseWhite);
   const [isInBag, setIsinBag] = useState(userBag.some(bag => bag.productId === id));
 
   useEffect(() => {
@@ -28,9 +30,9 @@ const BtnAddToBag = ({theme, id}) => {
   return (
     <button className={`${styles.addBagBtn} ${styles[themeClassName]}`} onClick={handleClick}>
         {isInBag ?
-          <img src={Close} alt="Remove from Bag" />
+          <img src={imageClose} alt="Remove from Bag" />
           :
-          <img src={image} alt="Add to Bag" />
+          <img src={imageBag} alt="Add to Bag" />
         }
         {isInBag ?
           <span>Remove from bag</span>
