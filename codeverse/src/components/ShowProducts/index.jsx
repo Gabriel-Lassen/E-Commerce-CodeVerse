176374c1657ad0ba28b1886dfa9de2ad11932dc9
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-07 11:09:35 -0300

    add: ProductsByCategory initial structure ShowProducts

---
+++ codeverse/src/components/ShowProducts/index.jsx
@@ -0,0 +1,47 @@
+import { useContext, useState, useEffect } from "react";
+import { ProductsContext } from "../../contexts/products";
+import ProductCard from '../ProductCard';
+
+const ShowProducts = ({category}) => {
+
+    const { listProducts } = useContext(ProductsContext);
+    const [totalProducts, setTotalProducts] = useState();
+    const [productsCategoryFiltred, setProductsCategoryFiltred] = useState();
+
+    useEffect(() => {
+        if(listProducts){
+          const list = listProducts.filter((product) => {return product.category.indexOf(category) !== -1});
+          setTotalProducts(list.length);
+          setProductsCategoryFiltred(list)
+        }
+      }, [listProducts, category])
+
+  return (
+    <div>
+        <span>{totalProducts} Products</span>
+        <div>
+            {productsCategoryFiltred && 
+                productsCategoryFiltred.map((item ,idx) => {
+                    return <ProductCard
+                        key={idx}
+                        id={item.id}
+                        name={item.name}
+                        info={item.info}
+                        price={item.price}
+                        discount={item.discount}
+                        averageStars={item.rating.averageStars}
+                        totalRatings={item.rating.totalRatings}
+                        url={item.url}
+                        popularity={item.popularity}
+                        reviews={item.reviews}
+                        addToBagBtn={true}
+                        rating={true}
+                    />
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default ShowProducts
--- codeverse/src/pages/ProductsByCategory/index.jsx
+++ codeverse/src/pages/ProductsByCategory/index.jsx
@@ -1,8 +1,24 @@
-
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import BtnBackForPage from "../../components/BtnBackForPage";
+import ShowProducts from "../../components/ShowProducts";
 
 const ProductsByCategoy = () => {
+  const location = useLocation();
+  const [category, setCategory] = useState();
+
+  useEffect(() => {
+    const categoryUrl = window.location.pathname.split("/categories/").pop();
+    const capitalized = categoryUrl[0].toUpperCase() + categoryUrl.substr(1);
+    setCategory(capitalized)
+  }, [location])
+
+
   return (
-    <div>ProductsByCategoy</div>
+    <div>
+      {/*<BtnBackForPage text={category} />*/}
+      <ShowProducts category={category} />
+    </div>
   )
 }
 
