2550eb3670e34f7dc8c8a577abd8a58181912757
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-08 13:09:59 -0300

    add: ProductsByCategory component

---
+++ codeverse/src/components/ProductsByCategory/index.jsx
@@ -0,0 +1,9 @@
+import ProductsByCategoyMobile from './ProductsByCategoryMobile';
+
+const ProductsByCategoy = () => {
+  return (
+    <ProductsByCategoyMobile />
+  )
+}
+
+export default ProductsByCategoy
