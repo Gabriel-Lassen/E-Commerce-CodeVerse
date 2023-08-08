5c0d70a241f03e0126183752cb4ab32a9764327b
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-08 17:53:24 -0300

    style: ProducstSortByMobile change div for button

--- codeverse/src/components/ProductsSortByMobile/index.jsx
+++ codeverse/src/components/ProductsSortByMobile/index.jsx
@@ -10,30 +10,30 @@ const ProductsSortByMobile = () => {
     const {SortBy, setSortBy} = useContext(FilterActionsContext);
   return (
     <div className={styles.wrapper}>
-        <div className={styles.option} onClick={() => setSortBy('Popular')}>
+        <button className={styles.option} onClick={() => setSortBy('Popular')}>
             <img src={SortBy === 'Popular' ? Active : Inactive} alt="Select" />
             <span>Popular Products</span>
-        </div>
-        <div className={styles.option} onClick={() => setSortBy('Relevance')}>
+        </button>
+        <button className={styles.option} onClick={() => setSortBy('Relevance')}>
             <img src={SortBy === 'Relevance' ? Active : Inactive} alt="Select" />
             <span>Relevance</span>
-        </div>
-        <div className={styles.option} onClick={() => setSortBy('New')}>
+        </button>
+        <button className={styles.option} onClick={() => setSortBy('New')}>
             <img src={SortBy === 'New' ? Active : Inactive} alt="Select" />
             <span>What's New</span>
-        </div>
-        <div className={styles.option} onClick={() => setSortBy('PriceLowToHigh')}>
+        </button>
+        <button className={styles.option} onClick={() => setSortBy('PriceLowToHigh')}>
             <img src={SortBy === 'PriceLowToHigh' ? Active : Inactive} alt="Select" />
             <span>Price- Low to High</span>
-        </div>
-        <div className={styles.option} onClick={() => setSortBy('PriceHighToLow')}>
+        </button>
+        <button className={styles.option} onClick={() => setSortBy('PriceHighToLow')}>
             <img src={SortBy === 'PriceHighToLow' ? Active : Inactive} alt="Select" />
             <span>Price- High to Low</span>
-        </div>
-        <div className={styles.option} onClick={() => setSortBy('OnSale')}>
+        </button>
+        <button className={styles.option} onClick={() => setSortBy('OnSale')}>
             <img src={SortBy === 'OnSale' ? Active : Inactive} alt="Select" />
             <span>On Sale</span>
-        </div>
+        </button>
     </div>
   )
 }
