23d5b94f661ca733b99d13276a953ec7175c5c47
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-08 14:12:03 -0300

    add: FilterBrands structure and style

---
+++ codeverse/src/components/ProductsFilterComponents/FilterBrands/index.jsx
@@ -0,0 +1,37 @@
+import { useContext, useState } from "react";
+import { ProductsContext } from "../../../contexts/products";
+
+import styles from './styles.module.scss';
+
+import Checkbox from '../../../assets/imgs/checkbox.svg';
+import CheckboxActive from '../../../assets/imgs/checkbox-active.svg';
+
+const FilterBrands = () => {
+  const { listBrands } = useContext(ProductsContext);
+  const [filtredBrands, setFiltredBrands] = useState([]);
+
+  function handleClick(brand){
+    if(filtredBrands.includes(brand)){
+      setFiltredBrands(filtredBrands.filter((b) => b !== brand))
+    } else {
+      setFiltredBrands([...filtredBrands, brand])
+    }
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      {listBrands && 
+        listBrands.map((brand) => {
+          return (
+            <div onClick={() => handleClick(brand)} key={brand} className={styles.option}>
+              <img src={filtredBrands.includes(brand) ? CheckboxActive : Checkbox} alt="Select" />
+              <span>{brand}</span>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default FilterBrands
