fdbe70bc45cafdd0c10d7ee35875014621cb7bbd
Nicolle Poltosi <nimendespoltosi46@gmail.com>
2023-08-09 16:56:59 -0300

    feat: styles and index wishlist

--- codeverse/src/components/MyWishlist/index.jsx
+++ codeverse/src/components/MyWishlist/index.jsx
@@ -1,9 +1,9 @@
 import { useContext, useState } from "react";
 import { WishlistActionsContext } from "../../contexts/wishlistActions";
 import { ProductsContext } from "../../contexts/products";
-import { BagActionsContext } from "../../contexts/bagActions";
 
 import styles from "./styles.module.scss";
+import BtnAddToBag from "../BtnAddToBag";
 
 const MyWishlist = () => {
   const { listProducts } = useContext(ProductsContext);
@@ -47,23 +47,30 @@ const MyWishlist = () => {
         const price = discountedPrice(item.productId);
 
         return (
-          <div key={item.productId}>
-            <img src={product.url} alt={product.name} />
+          <div className={styles.all} key={item.productId}>
+            <img
+              src={product.url}
+              className={styles.prodImg}
+              alt={product.name}
+            />
             <div className={styles.prodDesc}>
               <div className={styles.info}>
                 <span className={styles.name}>{product.name}</span>
-
                 <span className={styles.desc}>{product.info}</span>
               </div>
-
               <div className={styles.priceContainer}>
                 <h2>${price.toFixed(2)}</h2>
-
                 <span className={styles.originPrice}>
                   <s>{product.price}</s>
                 </span>
-
                 <p className={styles.off}>{product.discount * 100}% OFF</p>
+              </div>
+              <div className={styles.button}>
+                <BtnAddToBag
+                  theme={"light"}
+                  id={item.productId}
+                  width={"400px"}
+                />
               </div>
             </div>
           </div>
