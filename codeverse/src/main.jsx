0cffcd9d8ec5eec2b2c9f215196cc1288fa17b9a
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-10 17:25:17 -0300

    style: organize imports in main file

--- codeverse/src/main.jsx
+++ codeverse/src/main.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Rotas from "./routes/routes";
-import "./global.scss";
+import { BrowserRouter } from "react-router-dom";
+
 import ProductsProvider from "./contexts/products";
 import AuthProvider from "./contexts/Auth";
 import BagActionsProvider from "./contexts/bagActions";
-import WishlistActionsProvider, { WishlistActionsContext } from "./contexts/wishlistActions";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer, Slide } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css"
+import WishlistActionsProvider from "./contexts/wishlistActions";
 import OrdersActionsProvider from "./contexts/ordersActions";
+
+import Rotas from "./routes/routes";
+
+import { ToastContainer, Slide } from "react-toastify";
+
+import "./global.scss";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
