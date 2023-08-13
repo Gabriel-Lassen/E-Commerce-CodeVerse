a19631c1e198508e05635c092c9994d83b62aa43
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-13 14:16:09 -0300

    refactor: MyWishlistt display and show products qty

--- codeverse/src/components/MyWishlist/index.jsx
+++ codeverse/src/components/MyWishlist/index.jsx
@@ -18,31 +18,30 @@ const MyWishlist = () => {
     return <MyWishlistNotFound />;
   }
   return (
-    <div
-      className={
-        userWishlist.length % 2 == 0 ? styles.productsPar : styles.productsImpar
-      }
-    >
-      {userWishlist.map((item, idx) => {
-        const product = getProductById(item.productId);
-        return (
-          <ProductCard
-            key={idx}
-            id={product.id}
-            name={product.name}
-            info={product.info}
-            price={product.price}
-            discount={product.discount}
-            averageStars={product.rating.averageStars}
-            totalRatings={product.rating.totalRatings}
-            url={product.url}
-            popularity={product.popularity}
-            reviews={product.reviews}
-            addToBagBtn={true}
-            rating={true}
-          />
-        );
-      })}
+    <div className={styles.wrapper}>
+      <span className={styles.productsQty}>{userWishlist.length} products</span>
+      <div className={userWishlist.length % 2 == 0 ? styles.productsPar : styles.productsImpar}>
+        {userWishlist.map((item, idx) => {
+          const product = getProductById(item.productId);
+          return (
+            <ProductCard
+              key={idx}
+              id={product.id}
+              name={product.name}
+              info={product.info}
+              price={product.price}
+              discount={product.discount}
+              averageStars={product.rating.averageStars}
+              totalRatings={product.rating.totalRatings}
+              url={product.url}
+              popularity={product.popularity}
+              reviews={product.reviews}
+              addToBagBtn={true}
+              rating={true}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
