cbbfd82508e65c9c49d2744d9c3eff2bf91214e9
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-13 16:58:04 -0300

    refactor: not found bag style

--- codeverse/src/components/NotFoundMyBag/index.jsx
+++ codeverse/src/components/NotFoundMyBag/index.jsx
@@ -2,8 +2,12 @@ import styles from './styles.module.scss'
 import BtnBackForPage from "../BtnBackForPage";
 import ArrowSvg from "../ArrowSvg";
 import bag from '../../assets/imgs/bagNotFound.png'
+import MobileFixedBottomBar from '../MobileFixedBottomBar';
+import BtnGeneric from '../BtnGeneric';
+import { useNavigate } from 'react-router-dom';
 
 const MyBagFound = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
         <BtnBackForPage
@@ -14,8 +18,15 @@ const MyBagFound = () => {
             <img src={bag} alt="" />
             <h2>Uh Oh....!</h2>
             <p>You haven’t added any any items. Start shopping to make your bag bloom</p>
-            <button>Continue Shopping</button>
+            <div className={styles.btnDesktop}>
+              <BtnGeneric theme='dark' text='Continue shopping' onClick={() => navigate('/')} />
+            </div>
         </div>
+        <MobileFixedBottomBar>
+          <div className={styles.btnWrapper}>
+            <BtnGeneric theme='dark' text='Continue shopping' onClick={() => navigate('/')} />
+          </div>
+        </MobileFixedBottomBar>
     </div>
   )
 }
