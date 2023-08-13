b3fcab9b823409926249c7a93e947eb1cae55fb9
Samuel Golnik <samuelgolnikfilho@gmail.com>
2023-08-13 17:05:13 -0300

    refactor: not found wishlist style

--- codeverse/src/components/NotFoundMyWishlist/index.jsx
+++ codeverse/src/components/NotFoundMyWishlist/index.jsx
@@ -2,8 +2,13 @@ import styles from "./styles.module.scss";
 import BtnBackForPage from "../BtnBackForPage";
 import ArrowSvg from "../ArrowSvg";
 import imgWishlist from "../../assets/imgs/imgWishlist.png";
+import MobileFixedBottomBar from '../MobileFixedBottomBar';
+import BtnGeneric from '../BtnGeneric';
+import { useNavigate } from 'react-router-dom';
+
 
 const MyWishlistNotFound = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
       <BtnBackForPage
@@ -16,8 +21,15 @@ const MyWishlistNotFound = () => {
           <h2>Well...</h2>
           <p>It seems you have not added any products to for wishlist. </p>
         </div>
-        <button>Start Shopping</button>
+        <div className={styles.btnDesktop}>
+          <BtnGeneric theme='dark' text='Continue shopping' onClick={() => navigate('/')} />
+        </div>
       </div>
+      <MobileFixedBottomBar>
+          <div className={styles.btnWrapper}>
+            <BtnGeneric theme='dark' text='Continue shopping' onClick={() => navigate('/')} />
+          </div>
+        </MobileFixedBottomBar>
     </div>
   );
 };
