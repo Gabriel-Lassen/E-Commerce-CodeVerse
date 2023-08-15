940c35eaabe1107a2d81265314c260f21e33e98a
Gabriel Lassen dos Santos <gabriel.lassen2005@gmail.com>
2023-08-15 16:58:49 -0300

    add: Checkout Page Mobile

--- codeverse/src/pages/Checkout/index.jsx
+++ codeverse/src/pages/Checkout/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+
 import OrderSumary from "../../components/OrderSumary"
 import Header from "../../components/Header";
 import ShowFooter from "../../components/ShowFooter";
@@ -7,12 +8,14 @@ import ArrowSvg from "../../components/ArrowSvg";
 import TitlePage from "../../components/Title-pages";
 import RouteHistory from "../../components/Route-history";
 import Address from "../../components/Address";
-import styles from "./styles.module.scss";
-
 import DropdowBtn from "../../components/DropdowBtn";
 import BtnAddress from "../../components/BtnOpenAddress";
 import { Payment } from "../../components/Payment";
 import { PaymentDesktop } from "../../components/PaymentDesktop";
+import MobileSeparator from "../../components/MobileSeparator";
+
+import styles from "./styles.module.scss";
+
 
 
 const Checkout = () => {
@@ -39,25 +42,35 @@ const Checkout = () => {
         />
         <RouteHistory />
         <TitlePage />
-        <div className={styles.wrapper}>
-        <div className={styles.info}>
-        <BtnAddress />
-        {hidden && 
-        <DropdowBtn title='Contact Information'>
-              <Address />
-        </DropdowBtn>}
-        
-        {hidden && 
-        <DropdowBtn title='Select Payment Method' footer='footer'>
-              <PaymentDesktop/>
-        </DropdowBtn>}
 
-        <Payment/>
+        <div className={styles.wrapper}>
+          <div className={styles.info}>
+          <DropdowBtn title='Contact Information'>
+                <Address />
+          </DropdowBtn>
+          
+          <DropdowBtn title='Select Payment Method' footer='footer'>
+                <PaymentDesktop/>
+          </DropdowBtn>
+          </div>
+           <OrderSumary/>
         </div>
-        
-        <OrderSumary/>
+
+        <div className={styles.mobile}>
+        <BtnAddress />
+
+        <MobileSeparator/>
+        <DropdowBtn title='Product Details'>
+          <OrderSumary/>
+        </DropdowBtn>
+
+        <MobileSeparator/>
+        <DropdowBtn title='Select Payment Method'>
+          <Payment/>
+        </DropdowBtn>
         </div>
      </div>
+
      {hidden && <ShowFooter />}
    </div>
   )
